79d0deb64cf7301eeaf5241e31c755e0586cd621
T-Midnight <belousovatanya1995@yandex.ru>
2019-05-16 01:15:08 +0400

    add insert and findAll functions

--- app/src/main/java/com/androidtranslator/data/HistoryContract.java
+++ app/src/main/java/com/androidtranslator/data/HistoryContract.java
@@ -1,5 +1,8 @@
 package com.androidtranslator.data;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
 //контейнер для базы данных
@@ -17,5 +20,28 @@ public class HistoryContract {
         public final static String COLUMN_TRANSLATE = "translate";
         public final static String COLUMN_LANG_FROM = "langFrom";
         public final static String COLUMN_LANG_TO = "langTo";
+
+        public static void insertTestData(DbHelper mDbHelper) {
+            SQLiteDatabase db = mDbHelper.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_WORD, "Привет");
+            values.put(COLUMN_TRANSLATE, "Hello");
+            values.put(COLUMN_LANG_FROM, "ru");
+            values.put(COLUMN_LANG_TO, "en");
+
+            long newRowId = db.insert(TABLE_NAME, null, values);
+        }
+
+        public static Cursor findAll(DbHelper mDbHelper) {
+            SQLiteDatabase db = mDbHelper.getReadableDatabase();
+            String[] projection = {
+                    _ID, COLUMN_WORD, COLUMN_TRANSLATE, COLUMN_LANG_FROM, COLUMN_LANG_TO
+            };
+
+            Cursor cursor = db.query(TABLE_NAME, projection, null, null,
+                    null, null, null);
+
+            return cursor;
+        }
     }
 }
