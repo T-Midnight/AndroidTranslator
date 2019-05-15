0ca82faaec4f3611ea2f64393bd7f0c29f7104b2
T-Midnight <belousovatanya1995@yandex.ru>
2019-05-16 01:18:00 +0400

    add intent

--- app/src/main/java/com/androidtranslator/MainActivity.java
+++ app/src/main/java/com/androidtranslator/MainActivity.java
@@ -1,17 +1,27 @@
 package com.androidtranslator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.androidtranslator.data.DbHelper;
+import com.androidtranslator.data.HistoryContract;
+
+import static com.androidtranslator.data.HistoryContract.*;
 
 public class MainActivity extends AppCompatActivity {
     private final String API_KEY = "trnsl.1.1.20190503T132704Z.4e36b17ed2836893.5b0339809720cab8b47a4f74fae8390374e0209d";
     Button button;
     EditText etFrom;
     EditText etTo;
+    ImageButton imageButton;
+
+    private DbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +30,23 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.btnTranslate);
         etFrom = (EditText) findViewById(R.id.etFrom);
         etTo = (EditText) findViewById(R.id.etTo);
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+
+        mDbHelper = new DbHelper(this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!etFrom.getText().toString().equals("")) {
-                    etTo.setText(etFrom.getText().toString());
-                }
+                HistoryEntry.insertTestData(mDbHelper);
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(intent);
             }
         });
     }
-
 }
