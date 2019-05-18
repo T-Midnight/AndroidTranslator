63c1817f864f9bf51048013f39b777d20d0a4116
T-Midnight <belousovatanya1995@yandex.ru>
2019-05-18 13:25:06 +0400

    add call adapter

--- app/src/main/java/com/androidtranslator/web/Client.java
+++ app/src/main/java/com/androidtranslator/web/Client.java
@@ -1,6 +1,7 @@
 package com.androidtranslator.web;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Client {
@@ -12,6 +13,7 @@ public class Client {
             INSTANCE = new Retrofit.Builder()
                     .baseUrl(ROOT)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build();
         }
 
