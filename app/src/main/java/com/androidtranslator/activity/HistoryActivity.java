a84c22c40e9587504527ea6dd67fdb2cd7cfa919
T-Midnight <belousovatanya1995@yandex.ru>
2019-05-19 15:23:32 +0400

    add back button

--- app/src/main/java/com/androidtranslator/activity/HistoryActivity.java
+++ app/src/main/java/com/androidtranslator/activity/HistoryActivity.java
@@ -3,6 +3,9 @@ package com.androidtranslator.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.androidtranslator.R;
@@ -19,6 +22,7 @@ public class HistoryActivity extends AppCompatActivity {
     TextView tVCount;
     TextView tVItems;
     List<String> historyItems = new ArrayList<>();
+    ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,15 @@ public class HistoryActivity extends AppCompatActivity {
         tVCount = (TextView) findViewById(R.id.tVCount);
         tVItems = (TextView) findViewById(R.id.tVItems);
         mDbHelper = new DbHelper(this);
+        btnBack = (ImageButton) findViewById(R.id.btnBack);
+        tVItems.setMovementMethod(new ScrollingMovementMethod());
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         showAll(mDbHelper);
     }
